e8ce5610fe39e6327d2143e08e4b6564d5cc1a61
Romanovitch <rokos.job.ru@gmail.com>
2020-10-05 23:07:04 +0300

    fix: rewrite whole function

--- src/index.js
+++ src/index.js
@@ -4,104 +4,86 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
-    let exprForCheck = expr.replace(/ /g, '')
-    .replace(/[^()]/g, '');
-exprForCheck = checkBrackets(exprForCheck, '()');
-if (exprForCheck) {
-    throw new Error('ExpressionError: Brackets must be paired');
-} else {
-    let arrCalc = expr
-        .split(/(\d+|[+-/*()])/)
-        .map(el => el.trim() )
-        .filter(el => el !== '')
-        .map(el => Number.isNaN(+el) ? el : +el); 
-    const priorForMinus = {
-        '(': 1,
-        ')': 1,
-        '/': 1,
-        '*': 1,
-        '+': 2,
-        '-': 2,
-    }
-for(var i = 0; i <= arrCalc.length; i++) {
-    if (
-        (arrCalc[i] === '-' && !Number.isNaN(arrCalc[i+1])) 
-        && (priorForMinus[arrCalc[i]] === priorForMinus[arrCalc[i+2]])
-    ) {
-        let newElem = -1 * arrCalc[i+1];
-        arrCalc.splice(i, 2, '+', newElem);
-    }
-}
-    let resNum = exprParser(arrCalc)
-    return resNum[0];
-}
-function exprParser(allArr){
-    let innArr = Array.from(allArr);
-    const prior = {
-        '(': 1,
-        ')': 1,
-        '/': 5,
-        '*': 4,
-        '+': 2,
-        '-': 3,
-    }
-for(var i = 0; i <= innArr.length; i++) {
-    let iMin1Elem = innArr[i-1];
-    let i0Elem = innArr[i];  
-    let i1Elem = innArr[i+1]; 
-    let i2Elem = innArr[i+2];  
-    let i3Elem = innArr[i+3]; 
-
-    if(typeof i0Elem == 'number' && typeof i2Elem == 'number') {
-        let ourIf01 = !!(iMin1Elem !== '/' && prior[iMin1Elem] <= prior[i1Elem]); // Этого Если знак Не равен "/" то -> приор знака Предыдущ меньше-равно 
-        let ourIf02 = !!(iMin1Elem === '/' && prior[iMin1Elem] <  prior[i1Elem]); // Этого Если знак Равен    "/" то -> приор знака Предыдущ меньше
-        let ourIf03 = !iMin1Elem; // Предыдущ знака Нет 
-        let ourIf04 = !!(prior[i1Elem] >= prior[i3Elem]); // приор Этого знака больше-равен Следующ
-        let ourIf05 = !i3Elem; // Следующ знака Нет
-        if( 
-            (ourIf03 || (ourIf01 || ourIf02)) && (ourIf05 || ourIf04)
-        ){
-            i0Elem = calculon(i0Elem, i1Elem, i2Elem);                
-            innArr.splice(i, 3, i0Elem);
-            if (innArr[i-1] === '(' && innArr[i+1] === ')'){
-                innArr.splice(i+1, 1); 
-                innArr.splice(i-1, 1);
+    const myReg = /[\*|\/|+|\-|(|)]|(\d+)/g
+    const primaryArray = expr.match(myReg).map(el => Number.isNaN(+el) ? el : +el)
+    const exitArray = []
+    const stackArray = []
+    const priorityRank = {
+                '(': 0,
+                ')': 0,
+                '+': 1,
+                '-': 1,
+                '/': 2,
+                '*': 2,
             }
-        } 
-    } 
-}
-
-if(innArr.length > 1) {
-    innArr = exprParser(innArr);
-}
-    return innArr
-}
-
-function checkBrackets(str, myConf){
-    str = str.replace(myConf, "");
-    return (str.includes(myConf) 
-      ? checkBrackets(str, myConf) 
-      : str);
-}
-
-function calculon(num1, oper, num2){
-    switch (true){
-        case ((oper === '/') && !num2):
-            throw new Error('TypeError: Division by zero.');
-        case (oper === '/'):
-            return num1 / num2;
-        case (oper === '*'):
-            return num1 * num2;
-        case (oper === '+'):
-            return num1 + num2
-        case (oper === '-'):
-            return num1 - num2;
+    function calculon(num1, num2, oper){
+        if((oper === '/') && !num2) throw new Error('TypeError: Division by zero.')
+        switch (oper){
+            case ('/'):
+                return num1 / num2
+                break
+            case ('*'):
+                return num1 * num2
+                break
+            case ('+'):
+                return num1 + num2
+                break
+            case ('-'):
+                return num1 - num2
+        }
+    }  
+    while (primaryArray.length) { //! проходим по всему первичному массиву
+        let elPrimaryArr = primaryArray.shift()  // достаем элемент из первичного массива
+        if (typeof elPrimaryArr === 'number') { //! может это число?
+            exitArray.push(elPrimaryArr) // пушим в выходной массив из первичного массива
+        } else if (elPrimaryArr === '(') { //! может это - "(" ?
+            stackArray.push(elPrimaryArr) // пушим в стэк
+        } else if (elPrimaryArr === ')') { //! а если это - ")" ?
+            while (stackArray.length) { // то проходим по всему(почти) стэку
+                let elStackArr = stackArray.pop() // достаем элемент из стэка
+                if (elStackArr !== '(') { // проверяем элемент стэка - если это не "("
+                    if (!stackArray.length) { // стэк уже закончился, а "(" все нет
+                        throw new Error('ExpressionError: Brackets must be paired')
+                    }
+                    exitArray.push(elStackArr) // то пушим в выходной массив из стэка
+                } else {
+                    break //! - если все-же "(", то закончим обход стэка
+                }
+            }
+        } else { //! ага, элемент это операция бинарная
+            while (stackArray.length) { // пройдемся по всему(почти) стэку
+                let elStackArr = stackArray.pop() // и достаем элемент из стэка
+                if (priorityRank[elStackArr] >= priorityRank[elPrimaryArr]) { // если стэк-приоритет >= элемент_примари_массива-приоритет
+                    exitArray.push(elStackArr) // то пушим в выходной массив из стэка
+                } else {
+                    stackArray.push(elStackArr) // не понадобился
+                    break
+                }
+            }
+            stackArray.push(elPrimaryArr) // и затем пушим в стэк
+        }
     }
-}
-
-
-}
+    //! а теперь все из стэка на выход, и заодно проверим скобочки
+    while (stackArray.length) {
+        let elStackArr = stackArray.pop() // достаем элемент из стэка
+        if (elStackArr === "(" || elStackArr === ")") {
+            throw new Error('ExpressionError: Brackets must be paired')
+        } else {
+            exitArray.push(elStackArr) // пушим в выходной массив элемент из стэка
+        }
+    }   
+    while (exitArray.length) { //! а теперь вычисляем
+        let elExitArr = exitArray.shift();
+        if (typeof elExitArr === 'number') { // может это число?
+            stackArray.push(elExitArr); // отправим в стэк
+        } else {
+            let elSecondStackArr = stackArray.pop(); // достаем элемент из стэка
+            let elFirstStackArr = stackArray.pop(); // достаем элемент из стэка
+            stackArray.push(calculon(elFirstStackArr, elSecondStackArr, elExitArr)) //! а теперь калькулён
+        }
+    }
+    return stackArray[0]
+  }
 
 module.exports = {
     expressionCalculator
